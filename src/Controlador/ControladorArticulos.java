6c835bc7348b8fbe108adfeeaa151ce9a999c06b
RaulValEsc <raulvalesc@gmail.com>
2021-12-01 21:28:53 +0100

    Initial commit

---
+++ src/Controlador/ControladorArticulos.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Modelo.Articulos;
+import java.util.ArrayList;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author raulv
+ */
+public class ControladorArticulos {
+
+    Session ss;
+    Transaction tx;
+
+    public ControladorArticulos() {
+    }
+    
+    public boolean anadirArticulos(Articulos a){
+        try{
+            iniciaOperacion();
+            ss.save(a);
+        }catch(HibernateException he){
+            System.out.println("Error = "+he.getMessage());
+            return false;
+        }finally{
+            cerrarOperacion();
+        }
+        return true;
+    }
+    
+    public boolean borrarArticulos(Articulos a){
+        try{
+            iniciaOperacion();
+            ss.delete(a);
+        }catch(HibernateException he){
+            System.out.println("Error = "+he.getMessage());
+            return false;
+        }finally{
+            cerrarOperacion();
+        }
+        return true;
+    }
+    
+    public boolean modificarArticulos(Articulos a){
+        try{
+            iniciaOperacion();
+            ss.update(a);
+        }catch(HibernateException he){
+            System.out.println("Error = "+he.getMessage());
+            return false;
+        }finally{
+            cerrarOperacion();
+        }
+        return true;
+    }
+    
+    public List<Articulos> filtrarArticulos(List<Articulos> lista, int campo, String filtro){
+        List<Articulos> listaFiltrada = new ArrayList();
+        switch (campo){
+            case 0:{
+                for(Articulos a : lista){
+                    if(a.getReferencia().startsWith(filtro)){
+                        listaFiltrada.add(a);
+                    }
+                }
+            }
+            case 1:{
+                for(Articulos a : lista){
+                if(a.getDescripcion().startsWith(filtro)){
+                    listaFiltrada.add(a);
+                }
+                }
+            }
+        }
+        return listaFiltrada;
+    }
+
+    private void iniciaOperacion() throws HibernateException {
+        ss = NewHibernateUtil.getSessionFactory().openSession();
+        tx = ss.beginTransaction();
+    }
+
+    private void cerrarOperacion() throws HibernateException {
+        tx.commit();
+        ss.close();
+    }
+}
---
+++ src/Controlador/ControladorBD.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+import Modelo.Articulos;
+import Modelo.Clientes;
+import Modelo.FacturasTot;
+import java.util.List;
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author raulv
+ */
+public class ControladorBD {
+    Session ss;
+    Transaction tx;
+    
+    public ControladorBD() {}
+    
+    public List<Articulos> cargarArticulos(){
+        iniciaOperacion();
+        String hql = "FROM Articulos AS A";
+        Query query = ss.createQuery(hql);
+        List results = query.list();
+        ss.close();
+        return results;
+    }
+    
+    public List<Clientes> cargarClientes(){
+        iniciaOperacion();
+        String hql = "FROM Clientes AS C";
+        Query query = ss.createQuery(hql);
+        List results = query.list();
+        ss.close();
+        return results;
+    }
+    
+    public List<FacturasTot> cargarFacturas(){
+        iniciaOperacion();
+        String hql = "FROM FacturasTot AS F";
+        Query query = ss.createQuery(hql);
+        List results = query.list();
+        ss.close();
+        return results;
+    }
+    
+    /*public List<FacturasTot> cargarOfertas(){
+        
+    }*/
+    
+    private void iniciaOperacion() throws HibernateException {
+        ss = NewHibernateUtil.getSessionFactory().openSession();
+        tx = ss.beginTransaction();
+    }
+
+    private void cerrarOperacion() throws HibernateException {
+        tx.commit();
+        ss.close();
+    }
+}
